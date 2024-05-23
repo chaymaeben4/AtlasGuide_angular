a6a1ce46c051664e045f891a585487f474e11d88
Youssafouhba <youssefouhba@gmail.com>
2024-05-23 04:03:56 +0100

    admin updates

--- src/app/environnment/environnment.ts
+++ src/app/environnment/environnment.ts
@@ -1,6 +1,6 @@
 export const environnment = {
   production: false,
-  authURL: "http://localhost:8080",
+  authURL: "https://citythrillsmorocco-1.onrender.com",
   adminURL: "http://localhost:8080/CityThrillsMorocco/Admin",
   apiURL: "http://localhost:8080/CityThrillsMorocco",
   }
